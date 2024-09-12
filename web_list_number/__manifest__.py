0486045ca9174cdb59d46e0a6e493156ae162ec7
clearzZ <1801226631@qq.com>
2024-09-12 13:58:07 +0800

    [ADD]-模块上传

---
+++ web_list_number/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Web List Number",
+    "summary": "列表序号 List Number",
+    "version": "17.0.0.1",
+    "category": "clearzZ/clearzZ",
+    "website": "mailto:1801226631@qq.com",
+    "author": "clearzZ",
+    "license": "AGPL-3",
+    "depends": ["base", "web"],
+    "sequence": 4,
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'web_list_number/static/src/views/list/*.*',
+        ]
+    },
+}
+
